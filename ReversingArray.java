ec3ee2a95787518a2955a6afe56f006609db7d05
PARISIBOYINA VIJAY VARDHAN <143518938+pvijay63@users.noreply.github.com>
2025-08-12 21:41:04 +0530

    Add files via upload

---
+++ ReversingArray.java
@@ -0,0 +1,15 @@
+package array_programs;
+
+public class ReversingArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+       int b[]= {3,-5,6,0,7};
+       for(int i=b.length-1;i>=0;i--)
+       {
+    	   System.out.print(b[i]+" ");
+       }
+	}
+
+}
